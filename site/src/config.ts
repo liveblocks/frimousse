360eaf03caa3163adad923b5b57da59df0a64ba4
Chris Nicholas <ctnicholasdev@gmail.com>
2025-03-19 12:36:47 +0000

    Update site/src/config.ts
    Co-authored-by: Marc Bouchenoire <bouchenoire.marc@gmail.com>

--- site/src/config.ts
+++ site/src/config.ts
@@ -2,7 +2,7 @@ export const config = {
   name: "Frimousse — An emoji picker for React",
   url: "https://frimousse.liveblocks.io",
   description:
-    "A lightweight, unstyled, and composable emoji picker for React. Frimousse is an open-source library that provides you with primitive components, originally created for Liveblocks Comments. Styles can be applied with CSS, Tailwind, JS-in-CSS, and more.",
+    "Frimousse is an open-source, lightweight, unstyled, and composable emoji picker for React—originally created for Liveblocks Comments. Styles can be applied with CSS, Tailwind CSS, CSS-in-JS, and more.",
   links: {
     twitter: "https://x.com/liveblocks",
     github: "https://github.com/liveblocks/frimousse",
